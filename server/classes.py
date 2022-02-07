f8e4fcb1be8a2fd4c47dbc21fba9c778e17b8132
BaconErie <77812176+BaconErie@users.noreply.github.com>
2022-02-07 10:05:03 -0500

    Add files via upload

---
+++ server/classes.py
@@ -0,0 +1,20 @@
+class Game:
+    def __init__(self, game_code, terms, host):
+        self.game_code = game_code
+        self.players = []
+        self.terms = []
+        self.host_socket_id = host
+        self.boards = []
+        self.current_board = []
+        self.words_displayed = 0
+        self.current_word = ''
+        self.terms_bucket = []
+        self.status = None # "Depreciated", even tho im the author. Use game.listen_for_answers
+        self.listen_for_answers = False
+
+class Player:
+    def __init__(self, socket_id, name):
+        self.name = name
+        self.socket_id = socket_id
+        self.score = 0
+        self.answer_chosen = None
